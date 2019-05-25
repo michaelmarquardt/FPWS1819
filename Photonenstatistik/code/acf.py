35dee5222d4dd106f328eb1a36f1f08d4f971344
Michael Marquardt <st143244@stud.uni-stuttgart.de>
2019-05-25 16:46:21 +0200

    Changed the calculation. Estimate the optimal count rate now

--- Photonenstatistik/code/acf.py
+++ Photonenstatistik/code/acf.py
@@ -32,9 +32,10 @@ omega   = 0.5*10**-9    # s
 B       = 1100.         # counts/s
 
 # NV center dependend properties
-N1      = [11000.,12000.,120000.]   # counts/s
-N2      = [20000.,21000.,200000.]   # counts/s
-T       = [6128.20,6525.34,460.99]  # s
+N1      = np.array([11000.,12000.,120000.])   # counts/s
+N2      = np.array([20000.,21000.,200000.])   # counts/s
+T       = np.array([6128.20,6525.34,460.99])  # s
+f       = N2/N1
 
 # preparing fit functions
 def g_exp(t,tau,a,h):
@@ -46,35 +47,45 @@ for k in (0,1):
     print("measurement {}".format(k+1))
     t, c    = np.loadtxt("../Photonenstatistik_Messdaten/messung_{}.dat".format(k+1),unpack=True)
     
+    # optimization of the count rates N1 and N2:
+    popt    = [0.,0.,2.]
+    while abs(popt[2]-1.) > 1e-7:
+        # calculations
+        S       = N1[k] + N2[k] - B
+        rho     = S/(S+B)
+        CN      = c/(N1[k]*N2[k]*omega*T[k])
+        g       = (CN-(1-rho**2))/rho**2
+        
+        # fit (separate for + and -)
+        p0  = [10.,g.min(),g.mean()]
+        popt,  pcov = curve_fit(g_exp,t,g,p0=p0)
+
+        # rescale N1 and N2
+        N1[k]  *= np.sqrt(popt[2])
+        N2[k]  *= np.sqrt(popt[2])
+    
+    print("   N1        = {}".format(N1[k]))
+    print("   N2        = {}".format(N2[k]))
+    
     # calculations
     S       = N1[k] + N2[k] - B
     print("    S        = {:n} counts/s".format(S))
     rho     = S/(S+B)
     print("    rho      = {:.3f}".format(rho))
     CN      = c/(N1[k]*N2[k]*omega*T[k])
-    #CN     /= CN.mean()
     g       = (CN-(1-rho**2))/rho**2
-    #g      /= g.mean()
     
     # fit (separate for + and -)
-    gp  = g[t>=0.]
-    tp  = t[t>=0.]
-    gm  = g[t<=0.]
-    tm  = t[t<=0.]
     p0  = [10.,g.min(),g.mean()]
-    poptp,  pcovp   = curve_fit(g_exp,tp,gp,p0=p0)
-    poptm,  pcovm   = curve_fit(g_exp,tm,gm,p0=p0)
-    print("    poptp    = {}".format(poptp))
-    print("    pcovp    = {}".format(np.diag(pcovp)))
-    print("    poptm    = {}".format(poptm))
-    print("    pcovm    = {}".format(np.diag(pcovm)))
+    popt,  pcov = curve_fit(g_exp,t,g,p0=p0)
+    print("    popt     = {}".format(popt))
+    print("    pcov     = {}".format(np.diag(pcov)))
+    
     
     # plot
-    tsp  = np.linspace(0,500,10000)
-    tsm  = np.linspace(-500,0,10000)
+    ts  = np.linspace(-500,500,10000)
     plt.plot(t,g,"x",label="Messdaten")
-    plt.plot(tsp,g_exp(tsp,poptp[0],poptp[1],poptp[2]),label="Fit +")
-    plt.plot(tsm,g_exp(tsm,poptm[0],poptm[1],poptm[2]),label="Fit -")
+    plt.plot(ts,g_exp(ts,popt[0],popt[1],popt[2]),label="Fit")
     plt.ylim((0,2))
     plt.xlabel(r"Zeitdifferenz $t$ [\si{\nano\second}]")
     plt.ylabel(r"Korrelationsfunktion $g$")
@@ -86,6 +97,22 @@ for k in (0,1):
 k   = 2
 print("measurement {}".format(k+1))
 t, c    = np.loadtxt("../Photonenstatistik_Messdaten/messung_{}.dat".format(k+1),unpack=True)
+
+# optimization of the count rates N1 and N2:
+g   = c
+while g.mean()-1. > 1e-7:
+    # calculations
+    S       = N1[k] + N2[k] - B
+    rho     = S/(S+B)
+    CN      = c/(N1[k]*N2[k]*omega*T[k])
+    g       = (CN-(1-rho**2))/rho**2
+
+    # rescale N1 and N2
+    N1[k]  *= np.sqrt(g.mean())
+    N2[k]  *= np.sqrt(g.mean())
+
+print("   N1        = {}".format(N1[k]))
+print("   N2        = {}".format(N2[k]))
 
 # calculations
 S       = N1[k] + N2[k] - B
